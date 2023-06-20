21d08929d965b169f1c7a6230a88f9fea0289612
sjamillah <j.ssozi@alustudent.com>
2023-06-20 16:12:20 +0200

    main.sh

--- main.sh
+++ main.sh
@@ -6,7 +6,10 @@ do
     read -rp "What operation would you like to perform {Create|View|Delete|Update|Exit}: " name
     case $name in
 	"Create")
-	    # Write creation code here
+	    read -rp "Enter student email:" email
+	    read -rp "Enter student age:" age
+	    read -rp "Enter student ID:" ID
+	    echo "$email $age $ID" >>students-list_0333.txt
 	    ;;
 	"Delete")
 	    # Write deletion code here
