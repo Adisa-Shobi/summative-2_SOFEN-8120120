74fa4963e6821ae9678d45d974478ad7ec627a36
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-06-24 12:21:08 +0000

    Boilerplate code for summative

---
+++ backup-0323-RW-SOFEN-8120120-15.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Script to back up the 0323-RW-SOFEN-8120120-15_q1 directory
+
+# Define remote directory
+# Define here
+# Backup files files to remote server using scp
+# Write code here
---
+++ move-to-directory.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Moves files to create backup directory
+
+# make the directory
+# Write code here
+# Moves selected files to the dir
+# Write code here
